5f5155d161015118a24c9c1eb897af6c6b3d1f8a
AnjuAsok <anjuaalakkal@gmail.com>
2022-08-22 19:52:44 +0530

    adminaddsubstudcourse

---
+++ Frontend/src/app/admn-login/admn-login.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { AdmnAuthService } from '../admn-auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-admn-login',
+  templateUrl: './admn-login.component.html',
+  styleUrls: ['./admn-login.component.css']
+})
+export class AdmnLoginComponent implements OnInit {
+
+  email!:string
+  password!:string
+
+  constructor(private admnauth:AdmnAuthService,private router:Router) { }
+
+  ngOnInit(): void {
+  }
+loginAdmin(){
+  const admin={
+    email:this.email,
+    password:this.password
+  }
+  
+ this.admnauth.admnlogin(admin)
+ .subscribe(data=>{
+  if(data as {success:true}){
+  
+    alert('valid');
+    this.router.navigate(['/admn-dashboard'])
+  }
+  else{
+    alert('invalid')
+  }
+  
+ })
+ }
+}
+ 
+
---
+++ Frontend/src/app/app.module.ts
@@ -0,0 +1,53 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http'
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { LandingComponent } from './landing/landing.component';
+import { EmployerComponent } from './employer/employer.component';
+import { EmpLoginComponent } from './emp-login/emp-login.component';
+import { EmpSignupComponent } from './emp-signup/emp-signup.component';
+import { StudentComponent } from './student/student.component';
+import { StudLoginComponent } from './stud-login/stud-login.component';
+import { StudSignupComponent } from './stud-signup/stud-signup.component';
+import { AdminComponent } from './admin/admin.component';
+import { AdmnDashboardComponent } from './admn-dashboard/admn-dashboard.component';
+import { AdmnLoginComponent } from './admn-login/admn-login.component';
+import { AdmnCourseComponent } from './admn-course/admn-course.component';
+import { AdmnStudentsComponent } from './admn-students/admn-students.component';
+import { AdmnSubadmnComponent } from './admn-subadmn/admn-subadmn.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LandingComponent,
+    EmployerComponent,
+    EmpLoginComponent,
+    EmpSignupComponent,
+    StudentComponent,
+    StudLoginComponent,
+    StudSignupComponent,
+    AdminComponent,
+    AdmnDashboardComponent,
+    AdmnLoginComponent,
+    AdmnCourseComponent,
+    AdmnStudentsComponent,
+    AdmnSubadmnComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
